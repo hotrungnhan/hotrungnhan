9502817830a4b3c706418b1bbb8fb0d8aa5d3408
KudouSterain <hotrungnhan29@gmail.com>
2021-08-25 22:24:12 +0700

    ci

--- vue.config.js
+++ vue.config.js
@@ -4,32 +4,32 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-    pages: {
-        index: {
-            // entry for the page
-            entry: 'src/main.js',
-            // the source template
-            template: 'public/index.html',
-            // output as dist/index.html
-            filename: 'index.html',
-            // when using title option,
-            // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'Hồ Trung Nhân',
-            // chunks to include on this page, by default includes
-            // extracted common chunks and vendor chunks.
-            chunks: ['chunk-vendors', 'chunk-common', 'index']
-        },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Hồ Trung Nhân',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
-
-    pluginOptions: {
-      i18n: {
-        locale: 'en',
-        fallbackLocale: 'vi',
-        localeDir: 'locales',
-        enableLegacy: false,
-        runtimeOnly: false,
-        compositionOnly: false,
-        fullInstall: true
-      }
+  },
+  publicPath: '/hotrungnhan/',
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'vi',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
     }
+  }
 }
