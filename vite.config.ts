37d8f707251ac50c95bb723b307be4be4f130776
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2022-08-17 17:15:51 +0700

    update html

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
-
+import { createHtmlPlugin } from 'vite-plugin-html'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      exclude: ['node_modules/**'],
-    }),
-  ],
+export default defineConfig(({ command }) => {
+  // eslint-disable-next-line no-undef
+  const isDev = command == 'serve'
+  return {
+    plugins: [
+      react(),
+      eslint({
+        exclude: ['node_modules/**'],
+      }),
+      createHtmlPlugin({
+        minify: true,
+        entry: 'src/main.tsx',
+        inject: {
+          data: { dev: isDev },
+        },
+      }),
+    ],
+  }
 })
