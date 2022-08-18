03447fe3cd3c387b8f69fc56e851c2bea3601687
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2022-08-18 09:56:18 +0700

    optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import shimReactPdf from 'vite-plugin-shim-react-pdf'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -16,7 +15,6 @@ export default defineConfig(({ command }) => {
       eslint({
         exclude: ['node_modules/**'],
       }),
-      shimReactPdf(),
       createHtmlPlugin({
         minify: true,
         entry: 'src/main.tsx',
@@ -25,18 +23,5 @@ export default defineConfig(({ command }) => {
         },
       }),
     ],
-    build: {
-      commonjsOptions: {
-        include: ['tailwind.config.cjs'],
-      },
-    },
-    optimizeDeps: {
-      include: [path.resolve(__dirname, 'tailwind.config.cjs')],
-    },
-    resolve: {
-      alias: {
-        'tailwind.config.cjs': path.resolve(__dirname, 'tailwind.config.cjs'),
-      },
-    },
   }
 })
