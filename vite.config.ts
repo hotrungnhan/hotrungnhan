caaffb3abf0df45fef856fa46b63760d295e928e
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2022-09-13 00:36:48 +0700

    image minify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import viteImagemin from 'vite-plugin-imagemin'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   // eslint-disable-next-line no-undef
@@ -18,6 +19,33 @@ export default defineConfig(({ command }) => {
         entry: 'src/main.tsx',
         inject: {
           data: { dev: isDev },
+        },
+      }),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 20,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+          ],
         },
       }),
     ],
